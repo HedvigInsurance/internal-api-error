ffe470093eb494e14b9a987f9a877f5f10c6d6f6
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2020-06-22 15:58:22 +0200

    added ability to publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
     kotlin("jvm") version "1.3.61"
+    id("com.jfrog.bintray") version "1.8.5"
+    `maven-publish`
 }
 
+val versionName = "1.0.0"
+
 group = "com.hedvig"
-version = "1.0.0"
+version = versionName
 
 repositories {
     mavenCentral()
@@ -19,5 +23,49 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+fun MavenPom.addDependencies() = withXml {
+    asNode().appendNode("dependencies").let { depNode ->
+        configurations.compile.allDependencies.forEach {
+            depNode.appendNode("dependency").apply {
+                appendNode("groupId", it.group)
+                appendNode("artifactId", it.name)
+                appendNode("version", it.version)
+            }
+        }
+    }
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    classifier = "sources"
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    publications {
+        register("mavenJava", MavenPublication::class) {
+            from(components["java"])
+            artifact(sourcesJar.get())
+        }
+    }
+}
+
+bintray {
+    user = project.findProperty("bintrayUser").toString()
+    key = project.findProperty("bintrayKey").toString()
+    publish = true
+    setPublications("mavenJava")
+    pkg.apply {
+        repo = "hedvig-java"
+        name = "internal-api-error"
+        userOrg = "hedvig"
+        setLicenses("MIT")
+        vcsUrl = "https://github.com/HedvigInsurance/internal-api-error.git"
+        version.apply {
+            name = versionName
+            vcsTag = versionName
+        }
     }
 }
